004e81a882735a4a172d7f11017ae580d00cd404
William Duplenne <william.duplenne@hetic.net>
2016-03-08 17:55:26 +0100

    Step 2 : Board

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,105 @@
+/* Exercice 1 */
 var MyComponent = React.createClass({
 	render: function() {
-		return <div>My Component</div>;
+		return (
+			<div>
+				<h1>{this.props.text}</h1>
+				<p>{this.props.children}</p>
+			</div>
+			);
 	}
 });
 
-ReactDOM.render(<MyComponent />, document.getElementById('container'));
+ReactDOM.render(<div>
+				<MyComponent text="Hello World">
+				Hello too.
+				</MyComponent>
+				<MyComponent text="How are you?">
+				Fine and you?
+				</MyComponent>
+				</div>, $('#container_exo1')[0]);
+
+/* Exercice 2 */
+var Note = React.createClass({
+	getInitialState: function() {
+		return {editing: false}
+	},
+	edit: function() {
+		this.setState({editing: true});
+	},
+	save: function() {
+		this.props.onChange(this.refs.newText.value, this.props.index);
+		this.setState({editing: false});
+	},
+	remove: function() {
+		this.props.onRemove(this.props.index);
+	},
+	renderDisplay: function() {
+		return (
+			<div className="myClassNote">
+				<span onClick={this.edit}>
+				{this.props.children}
+				</span>
+				<p><button onClick={this.remove}>Remove</button></p>
+			</div>
+			);
+	},
+	renderForm: function() {
+		return (
+			<div className="myClassForm">
+			<textarea ref="newText" defaultValue={this.props.children}></textarea>
+			<p><button onClick={this.save}>Save</button></p>
+			</div>
+			);
+	},
+	render: function() {
+		if(this.state.editing) {
+			return this.renderForm();
+		} 
+		else {
+			return this.renderDisplay();
+		}
+	}
+});
+
+var Board = React.createClass({
+	getInitialState: function() {
+		return {
+			notes: []
+		}
+	},
+	add: function(text) {
+		var arr = this.state.notes;
+		arr.push(text);
+		this.setState({notes: arr});
+	},
+	update: function(newText, i) {
+		var arr = this.state.notes;
+		arr[i] = newText;
+		this.setState({notes: arr});
+	},
+	remove: function(i) {
+		var arr = this.state.notes;
+		arr.splice(i, 1);
+		this.setState({notes: arr});
+	},
+	eachNote: function(note, i) {
+		return (
+			<Note key={i}
+				index={i}
+				onChange={this.update}
+				onRemove={this.remove}
+			>{note}</Note>
+			);
+	},
+	render: function() {
+		return (
+				<div className="myBoard">
+					<button onClick={this.add.bind(null, "New note")}>Add notes</button>
+					{this.state.notes.map(this.eachNote)}
+				</div>
+			);
+	}
+});
+
+ReactDOM.render(<Board />, $('#container_exo2')[0]);
